91bd54dd2f400dbbf831222758fe326a60ca43fe
terehov-space <terehov.space@gmail.com>
2020-10-31 20:08:53 +0300

    Fix namespace controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\Admin\AdminUserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
